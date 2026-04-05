a02eaefbac038816cd4d006890b8f46c1f015e1d
2025-BSCPE-145 <laibaanwaa1361@gmail.com>
2026-04-05 19:17:45 +0500

    Update and rename java.cs to script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const elements = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+
+    if (top < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
