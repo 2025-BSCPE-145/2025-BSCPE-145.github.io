d04b85d00dba5b84557a2e31ef143d1340785d5e
2025-BSCPE-145 <laibaanwaa1361@gmail.com>
2026-04-05 18:14:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const text = "Web Developer | Student | Designer";
-let i = 0;
+const toggle = document.getElementById("themeToggle");
 
-function type() {
-  if (i < text.length) {
-    document.querySelector("p").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(type, 100);
-  }
-}
-
-type();
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
